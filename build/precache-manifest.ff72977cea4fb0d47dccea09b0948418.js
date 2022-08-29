33d5312a672ec1576d458912681c68565aeb9234
Selom Sallas-Mensah <sxs1558@cs.bham.ac.uk>
2022-08-29 19:15:34 +0100

    demo commit

---
+++ build/precache-manifest.ff72977cea4fb0d47dccea09b0948418.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccadb4b7d209daf756c3bcb3d8c4895e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "122d46b9ebb060c2fdb1",
+    "url": "/static/js/2.363bc6ad.chunk.js"
+  },
+  {
+    "revision": "5d87188692044a8e5725",
+    "url": "/static/js/main.f0361b97.chunk.js"
+  },
+  {
+    "revision": "0490578715db2a537464",
+    "url": "/static/js/runtime-main.6238ac68.js"
+  }
+]);
